325568012350f238096adf915eee4ee2a9ea9089
kishanr777 <pci113@PCI113.server7.com>
2018-09-24 17:20:14 +0530

    spring security social second commit

--- src/main/java/org/o7/planning/sbsocial/config/WebSecurityConfig.java
+++ src/main/java/org/o7/planning/sbsocial/config/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package org.o7.planning.sbsocial.config;
 
 import org.o7.planning.sbsocial.entity.AppRole;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
@@ -17,9 +19,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
-		auth.userDetailsService(userDetailsService);
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());;
 	}
 	
 	@Override
